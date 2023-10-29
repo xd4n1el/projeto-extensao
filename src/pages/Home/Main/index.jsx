f0b2062cf61fddb6d97a26246d041d2bc23fc358
Daniel Schaeffer <daneschaeffer@hotmail.com>
2023-10-29 20:16:30 -0300

    fix: text

--- src/pages/Home/Main/index.jsx
+++ src/pages/Home/Main/index.jsx
@@ -180,7 +180,7 @@ const MainPage = () => {
             impróprios;
           </Text>
           <Text>
-            - Dificuldade nos setores agrícolas de irregularidades climáticas;
+            - Dificuldade nos setores agrícolas por irregularidades climáticas;
           </Text>
         </Section>
 
